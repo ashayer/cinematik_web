8054da2a4f2988820379c7ddb430cda6c1c35510
Aran Hayer <ashayer@users.noreply.github.com>
2022-08-26 02:25:20 -0700

    liked button now properly updates heart

--- src/components/LikeMovieButton.tsx
+++ src/components/LikeMovieButton.tsx
@@ -1,6 +1,6 @@
 import { trpc } from "../utils/trpc";
 import { useRouter } from "next/router";
-import { useSession, getSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { useState } from "react";
 
 const LikeMovieButton = () => {
@@ -8,55 +8,65 @@ const LikeMovieButton = () => {
 
   const router = useRouter();
   const { movieId } = router.query;
-  const [movieIsLiked, setMovieIsLiked] = useState(false);
-  const userMovieLikes = trpc.useQuery([
-    "movie.check-if-movie-liked",
-    { userId: session?.user?.id as string, movieId: movieId as string },
-  ]);
+
+  const [localIsLiked, setLocalIsLiked] = useState(false);
+
+  const movieIsLiked = trpc.useQuery(
+    [
+      "movie.check-if-movie-liked",
+      { userId: session?.user?.id as string, movieId: movieId as string },
+    ],
+    { onSuccess: setLocalIsLiked },
+  );
 
   const likeMovieMutation = trpc.useMutation(["movie.like-movie-by-id"]);
   const dislikeMutation = trpc.useMutation(["movie.disliked-movie"]);
 
-  if (userMovieLikes.isLoading) return null;
+  console.log(movieIsLiked);
   return (
     <>
-      {movieIsLiked || userMovieLikes.data !== null ? (
-        <button
-          className="btn btn-circle"
-          onClick={() => {
-            console.log("disliking movie");
-            dislikeMutation.mutate({
-              userId: session?.user?.id as string,
-              movieId: movieId as string,
-            });
-            setMovieIsLiked(false);
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="red"
-            viewBox="0 0 23 24"
-            stroke="red"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-            />
-          </svg>
-        </button>
+      {movieIsLiked.data && localIsLiked ? (
+        <>
+          {!movieIsLiked.isRefetching && (
+            <button
+              className="btn btn-circle"
+              onClick={() => {
+                console.log("disliking movie");
+                setLocalIsLiked(false);
+                dislikeMutation.mutate({
+                  userId: session?.user?.id as string,
+                  movieId: movieId as string,
+                });
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="red"
+                viewBox="0 0 23 24"
+                stroke="red"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                />
+              </svg>
+            </button>
+          )}
+        </>
       ) : (
         <button
           className="btn btn-circle"
           onClick={() => {
             console.log("liking movie");
+            setLocalIsLiked(true);
+
             likeMovieMutation.mutate({
               userId: session?.user?.id as string,
               movieId: movieId as string,
             });
-            setMovieIsLiked(true);
           }}
         >
           <svg
