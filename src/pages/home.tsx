715b74a38c1dc2494a077d05fba2cb89dc7cf7fd
Aran Hayer <ashayer@users.noreply.github.com>
2022-08-25 21:58:20 -0700

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,19 @@
 import type { NextPage } from "next";
 import { trpc } from "../utils/trpc";
 import MovieGrid from "../components/MovieGrid";
+import testData from "../utils/tempMovieData";
+import MovieCarousel from "../components/MovieCarousel";
 const LandingPage: NextPage = () => {
-  const movies = trpc.useQuery(["movie.get-popular-movies"]);
+  //const movies = trpc.useQuery(["movie.get-popular-movies"]);
 
-  if (movies.isLoading) return null;
+  //if (movies.isLoading) return null;
 
   return (
-    <div>
-      <h1>Trending Now</h1>
-      {movies.data && <MovieGrid data={movies.data} />}
+    <div className="text-center">
+      <h1 className="font-bold text-6xl pb-5 text-white">Trending Now</h1>
+      {/* {movies.data && <MovieGrid data={movies.data} />} */}
+      {<MovieCarousel data={testData.slice(0, 10)} />}
+      {<MovieGrid data={testData} />}
     </div>
   );
 };
