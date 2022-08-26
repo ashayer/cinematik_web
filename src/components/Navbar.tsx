394143fbff84544406bce788ec949ba2963b500e
Aran Hayer <ashayer@users.noreply.github.com>
2022-08-26 00:35:05 -0700

    added styling to landing page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,47 +5,51 @@ import Link from "next/link";
 const Navbar = () => {
   const { data: session } = useSession();
   return (
-    <nav className=" px-2 sm:px-4 py-2.5  dark:bg-gray-900">
-      <div className="navbar bg-gray-900">
-        <div className="flex-1">
-          <Link href="/home">
-            <a className="btn btn-ghost normal-case text-xl">FYLM LIST</a>
-          </Link>
-        </div>
-        <div className="flex-none gap-1">
-          <div className="form-control">
-            <input type="text" placeholder="Search" className="input input-bordered" />
-          </div>
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                {session?.user?.image && (
-                  <Image
-                    src={session?.user?.image ?? ""}
-                    alt="User Profile Pic"
-                    width={50}
-                    height={50}
-                  />
-                )}
+    <>
+      {session?.user && (
+        <nav className=" px-2 sm:px-4 py-2.5 bg-gray-900">
+          <div className="navbar bg-gray-900">
+            <div className="flex-1">
+              <Link href="/home">
+                <a className="btn btn-ghost normal-case text-xl">FYLM LIST</a>
+              </Link>
+            </div>
+            <div className="flex-none gap-1">
+              <div className="form-control">
+                <input type="text" placeholder="Search" className="input input-bordered" />
               </div>
-            </label>
-            <ul
-              tabIndex={0}
-              className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <Link href={`user/1`}>
-                  <a>Profile</a>
-                </Link>
-              </li>
-              <li>
-                <a onClick={() => signOut({ callbackUrl: "/" })}>Logout</a>
-              </li>
-            </ul>
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                  <div className="w-10 rounded-full">
+                    {session?.user?.image && (
+                      <Image
+                        src={session?.user?.image ?? ""}
+                        alt="User Profile Pic"
+                        width={50}
+                        height={50}
+                      />
+                    )}
+                  </div>
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+                >
+                  <li>
+                    <Link href={`user/1`}>
+                      <a>Profile</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <a onClick={() => signOut({ callbackUrl: "/" })}>Logout</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </nav>
+        </nav>
+      )}
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,34 +6,74 @@ const LandingPage: NextPage = () => {
 
   return (
     <div>
-      <div>
-        <h1>
-          <strong>FiLM LIST</strong>
-        </h1>
-      </div>
-      <div>
-        <h1>See popular movies</h1>
-      </div>
-      <div>
-        <h1>Track your favorites</h1>
-      </div>
-      <div>
-        <h1>Find new films</h1>
-      </div>
-      <div>
-        <div>
-          {!session && (
-            <button
-              onClick={() => {
-                console.log("clicked");
-                signIn("google", { callbackUrl: "/home" });
-              }}
-            >
-              <h1>GET STARTED</h1>
-            </button>
-          )}
+      <div className="hero min-h-screen bg-base-200">
+        <div className="text-center flex flex-col">
+          <div className="max-w-2xl">
+            <div>
+              <h1 className="text-8xl font-bold">FYLM LIST</h1>
+            </div>
+            <div className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="lightblue"
+                className="w-20 h-20 m-4 mx-auto"
+              >
+                <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                <path
+                  fillRule="evenodd"
+                  d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+
+              <h1 className="text-5xl font-bold">See trending movies</h1>
+            </div>
+            <div className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="red"
+                className="w-20 h-20 m-4 mx-auto"
+              >
+                <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+              </svg>
+
+              <h1 className="text-5xl font-bold">Track your favorites</h1>
+            </div>
+            <div className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="white"
+                className="w-20 h-20 m-4 mx-auto"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1.5 5.625c0-1.036.84-1.875 1.875-1.875h17.25c1.035 0 1.875.84 1.875 1.875v12.75c0 1.035-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 011.5 18.375V5.625zm1.5 0v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5a.375.375 0 00-.375-.375h-1.5A.375.375 0 003 5.625zm16.125-.375a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5A.375.375 0 0021 7.125v-1.5a.375.375 0 00-.375-.375h-1.5zM21 9.375A.375.375 0 0020.625 9h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5zm0 3.75a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5zm0 3.75a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5zM4.875 18.75a.375.375 0 00.375-.375v-1.5a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5zM3.375 15h1.5a.375.375 0 00.375-.375v-1.5a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375zm0-3.75h1.5a.375.375 0 00.375-.375v-1.5A.375.375 0 004.875 9h-1.5A.375.375 0 003 9.375v1.5c0 .207.168.375.375.375zm4.125 0a.75.75 0 000 1.5h9a.75.75 0 000-1.5h-9z"
+                  clipRule="evenodd"
+                />
+              </svg>
+
+              <h1 className="text-5xl font-bold">Find new films</h1>
+            </div>
+            <div>
+              <div>
+                {!session && (
+                  <button
+                    className="btn btn-primary mt-5"
+                    onClick={() => {
+                      console.log("clicked");
+                      signIn("google", { callbackUrl: "/home" });
+                    }}
+                  >
+                    <h1>GET STARTED</h1>
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
-        <div>{session && <h1>{session.user?.name}</h1>}</div>
       </div>
     </div>
   );
