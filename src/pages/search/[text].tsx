fec6b9effaa00d76f14514fc900012d625792428
Aran Hayer <ashayer@users.noreply.github.com>
2022-08-26 13:55:46 -0700

    fixed no router error

--- src/pages/search/[text].tsx
+++ src/pages/search/[text].tsx
@@ -1,14 +1,24 @@
-import router from "next/router";
+import { useSession } from "next-auth/react";
+import router, { useRouter } from "next/router";
 import MovieSearchResults from "../../components/MovieSearchResults";
 import { trpc } from "../../utils/trpc";
 
 const Text = () => {
-  const { text } = router.query;
+  const { data: session } = useSession();
+
+  const nrouter = useRouter();
+  const { text } = nrouter.query;
 
   const searchResults = trpc.useQuery(["movie.search-movie", { searchText: text as string }]);
 
   if (searchResults.isLoading) return <div>Loading...</div>;
   if (searchResults.isError) return <div>Error...</div>;
+
+  if (!session?.user) {
+    router.push({
+      pathname: "/",
+    });
+  }
 
   return (
     <div>
