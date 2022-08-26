fa31ed16f7f4c966727051870c67ddaf61d234ee
Aran Hayer <ashayer@users.noreply.github.com>
2022-08-26 00:07:42 -0700

    added styling to movie details page

--- src/pages/movie/[movieId].tsx
+++ src/pages/movie/[movieId].tsx
@@ -3,6 +3,8 @@ import { trpc } from "../../utils/trpc";
 import { useRouter } from "next/router";
 import { useSession, getSession } from "next-auth/react";
 import LikeMovieButton from "../../components/LikeMovieButton";
+import { tempMovieDetail } from "../../utils/tempMovieDetails";
+import MovieDetailsOverview from "../../components/MoveDetails";
 
 const MovieDetails: NextPage = () => {
   const { data: session } = useSession();
@@ -10,20 +12,24 @@ const MovieDetails: NextPage = () => {
   const router = useRouter();
   const { movieId } = router.query;
 
-  const movieDetails = trpc.useQuery(["movie.get-movie-details", { id: movieId as string }]);
+  // const movieDetails = trpc.useQuery(["movie.get-movie-details", { id: movieId as string }]);
 
-  if (movieDetails.isLoading) return <div>Loading..</div>;
+  // if (movieDetails.isLoading) return <div>Loading..</div>;
 
-  if (movieDetails.isError) return <h1>Not found</h1>;
+  // if (movieDetails.isError) return <h1>Not found</h1>;
 
   return (
     <div>
-      {movieDetails.isSuccess && (
+      {/* {movieDetails.isSuccess && (
         <>
           <LikeMovieButton />
           <div>{JSON.stringify(movieDetails)}</div>
         </>
-      )}
+      )} */}
+      <>
+        {/* <LikeMovieButton /> */}
+        <MovieDetailsOverview movie={tempMovieDetail} />
+      </>
     </div>
   );
 };
