c76cf524903d918c2c2d360ac760e0d0d403ed15
Aran Hayer <ashayer@users.noreply.github.com>
2022-08-26 03:20:09 -0700

    Update OtherUserLikedMovies.tsx

--- src/components/OtherUserLikedMovies.tsx
+++ src/components/OtherUserLikedMovies.tsx
@@ -1,5 +1,16 @@
+import { trpc } from "../utils/trpc";
+import { useSession } from "next-auth/react";
+import router from "next/router";
+import MovieGrid from "../components/MovieGrid";
+
 const OtherUserLikedMovies = () => {
-  return <div>OtherUserLikedMovies</div>;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-white">
+        Viewing other users movies likes in the works :)
+      </h1>
+    </div>
+  );
 };
 
 export default OtherUserLikedMovies;
