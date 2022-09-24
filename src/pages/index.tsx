daef5006072a204842bb3eacc4a10738dd394c96
Aran Hayer <ashayer@users.noreply.github.com>
2022-09-23 17:06:14 -0700

    adjusted landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,14 +18,14 @@ const LandingPage: NextPage = () => {
         <div className="text-center flex flex-col">
           <div className="max-w-2xl">
             <div>
-              <h1 className="text-8xl font-bold">FYLM LIST</h1>
+              <h1 className="text-7xl font-bold">FYLM LIST</h1>
             </div>
             <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="lightblue"
-                className="w-20 h-20 m-4 mx-auto"
+                className="w-20 h-20 m-2 mx-auto"
               >
                 <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
                 <path
@@ -35,26 +35,26 @@ const LandingPage: NextPage = () => {
                 />
               </svg>
 
-              <h1 className="text-5xl font-bold">See trending movies</h1>
+              <h1 className="text-4xl font-bold">See whats trending</h1>
             </div>
             <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="red"
-                className="w-20 h-20 m-4 mx-auto"
+                className="w-20 h-20 m-2 mx-auto"
               >
                 <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
               </svg>
 
-              <h1 className="text-5xl font-bold">Track your favorites</h1>
+              <h1 className="text-4xl font-bold">Like your favorites</h1>
             </div>
             <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="white"
-                className="w-20 h-20 m-4 mx-auto"
+                className="w-20 h-20 m-2 mx-auto"
               >
                 <path
                   fillRule="evenodd"
@@ -63,7 +63,7 @@ const LandingPage: NextPage = () => {
                 />
               </svg>
 
-              <h1 className="text-5xl font-bold">Find new films</h1>
+              <h1 className="text-4xl font-bold">Discover new films</h1>
             </div>
             <div>
               <div>
