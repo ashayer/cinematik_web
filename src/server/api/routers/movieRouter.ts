1c66fef2ab4e7fc4806a1bc72ab0071f1c9c7a13
Aran Hayer <ashayer@users.noreply.github.com>
2023-01-27 22:18:31 -0800

    created movie router

--- src/server/api/routers/movieRouter.ts
+++ src/server/api/routers/movieRouter.ts
@@ -1,11 +1,99 @@
 import { z } from "zod";
-
+import { MovieDb } from "moviedb-promise";
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
 export const movieRouter = createTRPCRouter({
-  getMovieComments: protectedProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return "you can now see this secret message!";
+  getPopularMovies: publicProcedure.query(async ({}) => {
+    const movieDB = new MovieDb(process.env.MOVIE_DB_API_KEY as string);
+    const popular = await movieDB.moviePopular();
+    return popular.results;
+  }),
+  getMovieDetails: publicProcedure
+    .input(z.object({ movieId: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const movieDB = new MovieDb(process.env.MOVIE_DB_API_KEY as string);
+      const movieDetails = await movieDB.movieInfo(input.movieId);
+      return movieDetails;
+    }),
+  getCastList: publicProcedure
+    .input(z.object({ movieId: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const movieDB = new MovieDb(process.env.MOVIE_DB_API_KEY as string);
+      const credits = await movieDB.movieCredits(input.movieId);
+
+      return credits;
+    }),
+  searchMovieByText: publicProcedure
+    .input(z.object({ searchText: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const movieDB = new MovieDb(process.env.MOVIE_DB_API_KEY as string);
+      const parameters = {
+        query: input.searchText,
+        page: 1,
+      };
+      const movieSearchResults = await movieDB.searchMovie(parameters);
+      return movieSearchResults;
+    }),
+  likeMovieById: protectedProcedure
+    .input(z.object({ userId: z.string(), movieId: z.string() }))
+    .query(async ({ input, ctx }) => {
+      return await ctx.prisma.likedMovies.create({
+        data: {
+          userId: input.userId,
+          movieId: input.movieId,
+        },
+      });
+    }),
+  dislikeMovieById: protectedProcedure
+    .input(z.object({ userId: z.string(), movieId: z.string() }))
+    .query(async ({ input, ctx }) => {
+      return await ctx.prisma.likedMovies.delete({
+        where: {
+          userId_movieId: {
+            userId: input.userId,
+            movieId: input.movieId,
+          },
+        },
+      });
+    }),
+  checkIfMovieLiked: protectedProcedure
+    .input(z.object({ userId: z.string(), movieId: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const isLiked = await ctx.prisma.likedMovies.findFirst({
+        where: {
+          AND: [{ userId: input.userId }, { movieId: input.movieId }],
+        },
+        select: {
+          movieId: true,
+        },
+      });
+
+      if (isLiked) return true;
+      return false;
+    }),
+  getUserLikedMovies: protectedProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const movieIdArray = await ctx.prisma.likedMovies.findMany({
+        where: {
+          userId: {
+            equals: input.userId,
+          },
+        },
+        select: {
+          movieId: true,
+        },
+      });
+      const movieDB = new MovieDb(process.env.MOVIE_DB_API_KEY as string);
+      if (movieIdArray && movieIdArray.length > 0) {
+        const movieDetailsArray = Promise.all(
+          movieIdArray.map(
+            async (movieId) => await movieDB.movieInfo(movieId.movieId)
+          )
+        );
+        return movieDetailsArray;
+      } else {
+        return null;
+      }
     }),
 });
